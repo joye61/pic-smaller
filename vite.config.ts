c6151318eb95ab3447f071e93115d4012cebbb14
joye <zhoujing2@chelun.com>
2024-05-20 19:57:38 +0800

    Add subdirectory entry access support, for issue #8

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
