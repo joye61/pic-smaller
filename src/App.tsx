6eac9b00c1e52c525cd81e6b3fe8ebcac83ad1fd
joye <zhoujing2@chelun.com>
2024-05-29 21:01:53 +0800

    mobile test

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,22 @@ import { gstate } from "./global";
 import { ContextAction } from "./ContextAction";
 import { Analytics } from "@vercel/analytics/react";
 import { Loading } from "./components/Loading";
+import { useResponse } from "./media";
+import { useEffect } from "react";
 
 export const App = observer(() => {
+  const { isMobile } = useResponse();
+
+  useEffect(() => {
+    if (isMobile) {
+      let vConsole: any = null;
+      import("vconsole").then((result) => {
+        vConsole = new result.default({ theme: "dark" });
+      });
+      return () => vConsole?.destroy();
+    }
+  }, [isMobile]);
+
   return (
     <ConfigProvider
       locale={gstate.locale?.antLocale}
