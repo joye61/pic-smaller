5f0b4337102eee582c4021789508278e80fb3f0d
Curator <ramen.curator@gmail.com>
2024-05-30 16:44:41 +0800

    refactor: update type annotation for modules to React.FC
    - Ensures more accurate typing for dynamically imported React components.

--- src/modules.ts
+++ src/modules.ts
@@ -1,7 +1,7 @@
 import type { LocaleData } from "@/type";
-export const modules = import.meta.glob<{
-  default: React.FunctionComponentFactory<object>;
-}>("@/pages/**/index.tsx");
+export const modules = import.meta.glob<{ default: React.FC }>(
+  "@/pages/**/index.tsx",
+);
 export const locales = import.meta.glob<{ default: LocaleData }>(
   "@/locales/*.ts",
 );
