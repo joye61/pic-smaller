4a195a7e903cafd0162751e5d83fc5aa28496626
Curator <ramen.curator@gmail.com>
2024-05-30 15:56:50 +0800

    feat: add type support for dynamic import of locales

--- src/modules.ts
+++ src/modules.ts
@@ -1,2 +1,5 @@
+import type { LocaleData } from "@/type";
 export const modules = import.meta.glob("@/pages/**/index.tsx");
-export const locales = import.meta.glob("@/locales/*.ts");
+export const locales = import.meta.glob<{ default: LocaleData }>(
+  "@/locales/*.ts",
+);
