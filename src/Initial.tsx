21b9885683f6e99b79535583a6a4288689695577
Curator <ramen.curator@gmail.com>
2024-07-18 09:30:46 +0800

    refactor: extract resource loading logic into separate func in Initial component
    - Extracted the resource loading logic from the `useEffect` hook in
      `Initial` component into a new function `loadResources`.
    - Created a custom hook `useInit` to handle the initialization logic.
    - Improved code readability and reusability by separating concerns.

--- src/Initial.tsx
+++ src/Initial.tsx
@@ -7,27 +7,35 @@ import { gstate } from "./global";
 import { Indicator } from "./components/Indicator";
 import { avifCheck } from "./engines/support";
 
-export const Initial = observer(() => {
+const loadResources = () => {
+  const loadList: Array<Promise<any>> = [
+    import("jszip"),
+    fetch(new URL("./engines/png.wasm", import.meta.url)),
+    fetch(new URL("./engines/gif.wasm", import.meta.url)),
+    fetch(new URL("./engines/avif.wasm", import.meta.url)),
+    import("./engines/WorkerPreview?worker"),
+    import("./engines/WorkerCompress?worker"),
+  ];
+  const langs = Object.values(locales);
+  const pages = Object.values(modules);
+  for (const load of [...langs, ...pages]) {
+    loadList.push(load());
+  }
+  loadList.push(avifCheck());
+  return Promise.all(loadList);
+};
+
+const useInit = () => {
   useEffect(() => {
     (async () => {
-      const loadList: Array<Promise<any>> = [
-        import("jszip"),
-        fetch(new URL("./engines/png.wasm", import.meta.url)),
-        fetch(new URL("./engines/gif.wasm", import.meta.url)),
-        fetch(new URL("./engines/avif.wasm", import.meta.url)),
-        import("./engines/WorkerPreview?worker"),
-        import("./engines/WorkerCompress?worker"),
-      ];
-      const langs = Object.values(locales);
-      const pages = Object.values(modules);
-      for (const load of [...langs, ...pages]) {
-        loadList.push(load());
-      }
-      loadList.push(avifCheck());
-      await Promise.all(loadList);
+      await loadResources();
       initRouter();
     })();
   }, []);
+};
+
+export const Initial = observer(() => {
+  useInit();
 
   return (
     <Flex align="center" justify="center" className="__initial">
